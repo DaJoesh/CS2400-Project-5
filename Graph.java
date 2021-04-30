bbc7ba019b1b7aaee4ae6bcf53b36abde2169607
BuildTools <unconfigured@null.spigotmc.org>
2021-04-29 21:18:22 -0700

    Trying something out - Josh and Russell
    Co-authored-by: rdrickards <rdrickards@users.noreply.github.com>

--- Graph.java
+++ Graph.java
@@ -1,61 +1,81 @@
 //CS 2400 - Project 5
 //By Russell Rickards and Joshua Jenkins
-public class Graph<E> implements GraphInterface<E> {
-    private boolean[][] edges; // edges[i][j] is true if there is a vertex from i to j
-    private E[] labels; // labels[i] contains the label for vertex i
+import java.io.*;
+import java.util.*;
+public class Graph
+{
+    class GraphBFS
+    {
+	private int V; // No. of vertices
+	private LinkedList<Integer> adj[]; //Adjacency Lists
 
-    // Constructor: initialize a Graph with n vertices, no edges, and null labels
-    public Graph(int n) {
-        edges = new boolean[n][n]; // All values initially false
-        labels = (E[]) new Object[n]; // All values initially null
-    }
+	// Constructor
+	GraphBFS(int v)
+	{
+		V = v;
+		adj = new LinkedList[v];
+		for (int i=0; i<v; ++i)
+			adj[i] = new LinkedList();
+	}
 
-    // Add an edge
-    public void addEdge(int source, int target) {
-        edges[source][target] = true;
-    }
+	// Function to add an edge into the graph
+	void addEdge(int v,int w)
+	{
+		adj[v].add(w);
+	}
 
-    // Accessor method to get the label of a vertex of this Graph
-    public E getLabel(int vertex) {
-        return labels[vertex];
-    }
+	// prints BFS traversal from a given source s
+	void BFS(int s)
+	{
+		// Mark all the vertices as not visited(By default
+		// set as false)
+		boolean visited[] = new boolean[V];
 
-    // Test whether an edge exists
-    public boolean isEdge(int source, int target) {
-        return edges[source][target];
-    }
+		// Create a queue for BFS
+		LinkedList<Integer> queue = new LinkedList<Integer>();
 
-    // Obtain a list of neighbors of a specified vertex of this Graph
-    public int[] neighbors(int vertex) {
-        int i;
-        int count = 0;
-        int[] answer;
+		// Mark the current node as visited and enqueue it
+		visited[s]=true;
+		queue.add(s);
 
-        for (i = 0; i < labels.length; i++) {
-            if (edges[vertex][i])
-                count++;
-        }
-        answer = new int[count];
-        count = 0;
-        for (i = 0; i < labels.length; i++) {
-            if (edges[vertex][i])
-                answer[count++] = i;
-        }
-        return answer;
-    }
+		while (queue.size() != 0)
+		{
+			// Dequeue a vertex from queue and print it
+			s = queue.poll();
+			System.out.print(s+" ");
 
-    // Remove an edge
-    public void removeEdge(int source, int target) {
-        edges[source][target] = false;
-    }
+			// Get all adjacent vertices of the dequeued vertex s
+			// If a adjacent has not been visited, then mark it
+			// visited and enqueue it
+			Iterator<Integer> i = adj[s].listIterator();
+			while (i.hasNext())
+			{
+				int n = i.next();
+				if (!visited[n])
+				{
+					visited[n] = true;
+					queue.add(n);
+				}
+			}
+		}
+	}
 
-    // Change the label of a vertex of this Graph
-    public void setLabel(int vertex, E newLabel) {
-        labels[vertex] = newLabel;
-    }
+	// Driver method to
+	public static void main(String args[])
+	{
+		GraphBFS g = new GraphBFS(4);
 
-    // Accessor method to determine the number of vertices in this Graph
-    public int size() {
-        return labels.length;
-    }
+		g.addEdge(0, 1);
+		g.addEdge(0, 2);
+		g.addEdge(1, 2);
+		g.addEdge(2, 0);
+		g.addEdge(2, 3);
+		g.addEdge(3, 3);
+
+		System.out.println("Following is Breadth First Traversal "+
+						"(starting from vertex 2)");
+
+		g.BFS(2);
+	}
+}
 }
