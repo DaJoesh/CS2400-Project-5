e7d4e200efb3ce2bcdc11b4183c4e403d6af4c75
BuildTools <unconfigured@null.spigotmc.org>
2021-04-29 19:30:09 -0700

    beginning on Graph.java and GraphInterface - J&R

--- Graph.java
+++ Graph.java
@@ -1,2 +1,12 @@
 //CS 2400 - Project 5
 //By Russell Rickards and Joshua Jenkins
+public class Graph<T>
+{
+    private boolean[][] edges; // edges[i][j] is true of there is a vertex from i to j
+    private T[] labels; //labels[i] contains the label for vertex i
+    public Graph(int n)
+    {
+        edges = new boolean [n][n]; // All values initially false
+        labels = (T[]) new Object[n]; // All values are intitially full
+    }
+}
---
+++ GraphInterface.java
@@ -0,0 +1,11 @@
+public class GraphInterface {
+
+    public boolean isEdge(int source, int target);
+
+    public void addEdge(int source, int target);
+    getLabel(int vertex);
+    neighbors(int vertex);
+    removeEdge(int source, int target);
+    setLabel(int vertex, E newLabel);
+
+}
