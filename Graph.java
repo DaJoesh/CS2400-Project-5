86d4be2632445c2fedbf0b95d9925cccb6ba7fed
BuildTools <unconfigured@null.spigotmc.org>
2021-05-03 13:14:09 -0700

    breadthfirst 1.0 - Josh and Russell
    Co-authored-by: rdrickards <rdrickards@users.noreply.github.com>

--- Graph.java
+++ Graph.java
@@ -1,16 +1,13 @@
-//CS 2400 - Project 5
+//CS 2400 - Project 5 
 //By Russell Rickards and Joshua Jenkins
 import java.io.*;
 import java.util.*;
 public class Graph
 {
-    class GraphBFS
-    {
-	private int V; // No. of vertices
-	private LinkedList<Integer> adj[]; //Adjacency Lists
+	private int V;
+	private LinkedList<Integer> adj[];
 
-	// Constructor
-	GraphBFS(int v)
+	public Graph(int v)
 	{
 		V = v;
 		adj = new LinkedList[v];
@@ -18,64 +15,31 @@ public class Graph
 			adj[i] = new LinkedList();
 	}
 
-	// Function to add an edge into the graph
-	void addEdge(int v,int w)
+	public void addEdge(int v,int w)
 	{
 		adj[v].add(w);
 	}
 
-	// prints BFS traversal from a given source s
-	void BFS(int s)
+	public void breadthFirstTraversal(int s)
 	{
-		// Mark all the vertices as not visited(By default
-		// set as false)
-		boolean visited[] = new boolean[V];
-
-		// Create a queue for BFS
-		LinkedList<Integer> queue = new LinkedList<Integer>();
-
-		// Mark the current node as visited and enqueue it
-		visited[s]=true;
-		queue.add(s);
-
-		while (queue.size() != 0)
+		int visited[] = new int[V];
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		visited[s]=1;
+		list.add(s);
+		while (list.size() != 0)
 		{
-			// Dequeue a vertex from queue and print it
-			s = queue.poll();
+			s = list.poll();
 			System.out.print(s+" ");
-
-			// Get all adjacent vertices of the dequeued vertex s
-			// If a adjacent has not been visited, then mark it
-			// visited and enqueue it
 			Iterator<Integer> i = adj[s].listIterator();
 			while (i.hasNext())
 			{
 				int n = i.next();
-				if (!visited[n])
+				if (visited[n] = 0)
 				{
-					visited[n] = true;
+					visited[n] = 1;
 					queue.add(n);
 				}
 			}
 		}
 	}
-
-	// Driver method to
-	public static void main(String args[])
-	{
-		GraphBFS g = new GraphBFS(4);
-
-		g.addEdge(0, 1);
-		g.addEdge(0, 2);
-		g.addEdge(1, 2);
-		g.addEdge(2, 0);
-		g.addEdge(2, 3);
-		g.addEdge(3, 3);
-
-		System.out.println("Following is Breadth First Traversal "+
-						"(starting from vertex 2)");
-
-		g.BFS(2);
-	}
-}
 }
